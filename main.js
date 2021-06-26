b66df179001cc841fc285cbe23dd02f93cebfdf4
lin <leftwest576@gmail.com>
2021-06-26 15:58:29 +0800

    add userInput function

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ let blockData = [
 ]
 
 let soundSetData = [
-    { name: 'corrent', set: [1, 3, 5, 8] },
+    { name: 'correct', set: [1, 3, 5, 8] },
     { name: 'wrong', set: [2, 4, 5.5, 7] }
+]
+
+let levelDatas = [
+    '1324',
+    '1232114143',
+    '13214443214321'
 ]
 
 let Blocks = function (blockAssign, setAssign) {
@@ -62,12 +68,68 @@ Blocks.prototype.getAudioObject = function (pitch) {
     return new Audio("https://awiclass.monoame.com/pianosound/set/" + pitch + ".wav")
 }
 
-Blocks.prototype.playSet = function(type){
-    this.soundSet.find( set => set.name === type )
+Blocks.prototype.playSet = function (type) {
+    let sets = this.soundSet.find(set => set.name === type).sets
+    sets.forEach((obj) => {
+        obj.currentTime = 0
+        obj.play()
+    })
 }
 
-let blocks = new Blocks(blockData, soundSetData)
+let Game = function () {
+    this.block = blocks = new Blocks(blockData, soundSetData)
+    this.level = levelDatas
+    this.currentLevel = 0
+    this.playInterval = 400
+    this.mode = 'waiting'
+}
 
-blocks.flash('1')
+Game.prototype.startGame = function (answer) {
+    this.node = 'gamePlay'
+    this.answer = answer
+
+    let notes = this.answer.split('')
+    this.timer = setInterval(() => {
+        let char = notes.shift()
+        this.playNote(char)
+        if (notes.length === 0) {
+            this.startUserInput()
+            clearInterval(this.timer)
+        }
+        console.log(char);
+
+    }, this.playInterval)
+
+}
+
+Game.prototype.playNote = function (note) {
+    this.block.flash(note)
+}
+
+Game.prototype.startUserInput = function () {
+    this.userInput = ''
+    this.mode = 'userInput'
+}
+
+Game.prototype.userSendInput = function (inputChar) {
+    if (this.mode === 'userInput') {
+        let tempString = this.userInput + inputChar
+        this.playNote(inputChar)
+        if (this.answer.indexOf(tempString) === 0) {
+            console.log('nice');
+            if (this.answer === tempString) {
+                console.log('correct');
+            }
+        }
+        this.userInput += inputChar
+        console.log(tempString);
+    }
+}
+
+let game = new Game()
+
+game.startGame('12333')
+
+// blocks.playSet('wrong')
 
 console.log(blocks);
